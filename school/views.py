964e3acef66490c20b6afad5ce9571547a4d6438
dantechdevs <damnngwasi@gmail.com>
2025-01-23 23:44:39 +0300

    configuring template

--- school/views.py
+++ school/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-#create your views here 
+
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, 'Home/index.html')
+
