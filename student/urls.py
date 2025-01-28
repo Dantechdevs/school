7c1b80027766c0cb320ffaa4e5c1116a682820ac
dantechdevs <damnngwasi@gmail.com>
2025-01-28 13:17:54 +0300

    display of student list on dashboard

--- student/urls.py
+++ student/urls.py
@@ -3,4 +3,8 @@ from. import views
 urlpatterns = [
     path('', views.student_list, name='student_list'),
     path('add/', views.add_student, name='add_student'),
+    path('view/students/<slug:slug>/', views.view_student, name='view_student'),
+    
+    path('edit/<slug:slug>/', views.edit_student, name='edit_student'),
+    path('delete/<slug:slug>/', views.delete_student, name='delete_student'),
 ]
