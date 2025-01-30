9f59a193a8fcf7a31f2b33d303dcab628c53a18c
dantechdevs <damnngwasi@gmail.com>
2025-01-30 20:47:07 +0300

    added student list

--- student/views.py
+++ student/views.py
@@ -4,37 +4,40 @@ from .models import *
 from django.contrib import messages
 from .models import Student, Parent, Notification
 
+
 def create_notification(user, message):
     Notification.objects.create(user=user, message=message)
 
+
 # Create your views here.
+
 
 def add_student(request):
     if request.method == "POST":
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        student_id = request.POST.get('student_id')
-        gender = request.POST.get('gender')
-        date_of_birth = request.POST.get('date_of_birth')
-        student_class = request.POST.get('student_class')
-        religion = request.POST.get('religion')
-        joining_date = request.POST.get('joining_date')
-        mobile_number = request.POST.get('mobile_number')
-        admission_number = request.POST.get('admission_number')
-        section = request.POST.get('section')
-        student_image = request.FILES.get('student_image')
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        student_id = request.POST.get("student_id")
+        gender = request.POST.get("gender")
+        date_of_birth = request.POST.get("date_of_birth")
+        student_class = request.POST.get("student_class")
+        religion = request.POST.get("religion")
+        joining_date = request.POST.get("joining_date")
+        mobile_number = request.POST.get("mobile_number")
+        admission_number = request.POST.get("admission_number")
+        section = request.POST.get("section")
+        student_image = request.FILES.get("student_image")
 
         # Retrieve parent data from the form
-        father_name = request.POST.get('father_name')
-        father_occupation = request.POST.get('father_occupation')
-        father_mobile = request.POST.get('father_mobile')
-        father_email = request.POST.get('father_email')
-        mother_name = request.POST.get('mother_name')
-        mother_occupation = request.POST.get('mother_occupation')
-        mother_mobile = request.POST.get('mother_mobile')
-        mother_email = request.POST.get('mother_email')
-        present_address = request.POST.get('present_address')
-        permanent_address = request.POST.get('permanent_address')
+        father_name = request.POST.get("father_name")
+        father_occupation = request.POST.get("father_occupation")
+        father_mobile = request.POST.get("father_mobile")
+        father_email = request.POST.get("father_email")
+        mother_name = request.POST.get("mother_name")
+        mother_occupation = request.POST.get("mother_occupation")
+        mother_mobile = request.POST.get("mother_mobile")
+        mother_email = request.POST.get("mother_email")
+        present_address = request.POST.get("present_address")
+        permanent_address = request.POST.get("permanent_address")
 
         # save parent information
         parent = Parent.objects.create(
@@ -47,7 +50,7 @@ def add_student(request):
             mother_mobile=mother_mobile,
             mother_email=mother_email,
             present_address=present_address,
-            permanent_address=permanent_address
+            permanent_address=permanent_address,
         )
 
         # Save student information
@@ -64,10 +67,12 @@ def add_student(request):
             admission_number=admission_number,
             section=section,
             student_image=student_image,
-            parent=parent
+            parent=parent,
         )
         if request.user.is_authenticated:
-            create_notification(request.user, f"Added Student: {student.first_name} {student.last_name}")
+            create_notification(
+                request.user, f"Added Student: {student.first_name} {student.last_name}"
+            )
             messages.success(request, "Student added successfully")
             return redirect("student_list")
 
@@ -75,46 +80,44 @@ def add_student(request):
 
 
 def student_list(request):
-    student_list = Student.objects.select_related('parent').all()
+    student_list = Student.objects.select_related("parent").all()
     unread_notification = request.user.notification_set.filter(is_read=False)
-    context = {
-        'student_list': student_list,
-        'unread_notification': unread_notification
-    }
+    context = {"student_list": student_list, "unread_notification": unread_notification}
     return render(request, "students/students.html", context)
-
-
-
 
 
 def edit_student(request, slug):
     student = get_object_or_404(Student, slug=slug)
-    parent = student.parent if hasattr(student, 'parent') else None
+    parent = student.parent if hasattr(student, "parent") else None
     if request.method == "POST":
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        student_id = request.POST.get('student_id')
-        gender = request.POST.get('gender')
-        date_of_birth = request.POST.get('date_of_birth')
-        student_class = request.POST.get('student_class')
-        religion = request.POST.get('religion')
-        joining_date = request.POST.get('joining_date')
-        mobile_number = request.POST.get('mobile_number')
-        admission_number = request.POST.get('admission_number')
-        section = request.POST.get('section')
-        student_image = request.FILES.get('student_image') if request.FILES.get('student_image') else student.student_image
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        student_id = request.POST.get("student_id")
+        gender = request.POST.get("gender")
+        date_of_birth = request.POST.get("date_of_birth")
+        student_class = request.POST.get("student_class")
+        religion = request.POST.get("religion")
+        joining_date = request.POST.get("joining_date")
+        mobile_number = request.POST.get("mobile_number")
+        admission_number = request.POST.get("admission_number")
+        section = request.POST.get("section")
+        student_image = (
+            request.FILES.get("student_image")
+            if request.FILES.get("student_image")
+            else student.student_image
+        )
 
         # Retrieve parent data from the form
-        parent.father_name = request.POST.get('father_name')
-        parent.father_occupation = request.POST.get('father_occupation')
-        parent.father_mobile = request.POST.get('father_mobile')
-        parent.father_email = request.POST.get('father_email')
-        parent.mother_name = request.POST.get('mother_name')
-        parent.mother_occupation = request.POST.get('mother_occupation')
-        parent.mother_mobile = request.POST.get('mother_mobile')
-        parent.mother_email = request.POST.get('mother_email')
-        parent.present_address = request.POST.get('present_address')
-        parent.permanent_address = request.POST.get('permanent_address')
+        parent.father_name = request.POST.get("father_name")
+        parent.father_occupation = request.POST.get("father_occupation")
+        parent.father_mobile = request.POST.get("father_mobile")
+        parent.father_email = request.POST.get("father_email")
+        parent.mother_name = request.POST.get("mother_name")
+        parent.mother_occupation = request.POST.get("mother_occupation")
+        parent.mother_mobile = request.POST.get("mother_mobile")
+        parent.mother_email = request.POST.get("mother_email")
+        parent.present_address = request.POST.get("present_address")
+        parent.permanent_address = request.POST.get("permanent_address")
         parent.save()
 
         # update student information
@@ -131,18 +134,23 @@ def edit_student(request, slug):
         student.section = section
         student.student_image = student_image
         student.save()
-        create_notification(request.user, f"Updated Student: {student.first_name} {student.last_name}")
+        create_notification(
+            request.user, f"Updated Student: {student.first_name} {student.last_name}"
+        )
         messages.success(request, "Student updated successfully")
         return redirect("student_list")
 
-    return render(request, "students/edit-student.html", {'student': student, 'parent': parent})
+    return render(
+        request, "students/edit-student.html", {"student": student, "parent": parent}
+    )
 
 
 def view_student(request, slug):
-    student = get_object_or_404(Student, student_id=slug)
-    context = {
-        'student': student
-    }
+    
+    # student = get_object_or_404(Student, student_id=slug)
+    student = Student.objects.get(slug=slug)
+    print(student.student_image)
+    context = {"student": student}
     return render(request, "students/student-details.html", context)
 
 
@@ -154,5 +162,5 @@ def delete_student(request, slug):
         if request.user.is_authenticated:
             create_notification(request.user, f"Deleted student: {student_name}")
             messages.success(request, "Student deleted successfully")
-            return redirect('student_list')
+            return redirect("student_list")
     return HttpResponseForbidden()
